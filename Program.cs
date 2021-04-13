f2e8adb8ba444ec426aa592e1cd88097ef1262ff
vicco <viccobara05@students.amikom.ac.id>
2021-04-13 15:13:48 +0700

    responsi

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman3407
+{
+    0references
+    class Program;
+
+        0references
+        static void Main(string[] args)
+    {
+        Karyawan paijo = new Karyawan();
+        Karyawan jono = new Karyawan();
+
+        paijo.nama = "Paijo";
+        paijo.NIK = 190302123;
+        paijo.gajibulanan = "3,000,000";
+
+        jono.nama = "Jono";
+        jono.NIK = 190302124;
+        jono.gajibulanan = "2,000,000";
+
+        Console.WriteLine("NIK/Nama\tGajibulanan");
+        Console.WriteLine("-----------------------------")
+
+        paijo.bulanan();
+        jono.bulanan();
+
+
+        Console.ReadKey();
+    }
+}
---
+++ karyawan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ResponsiPemrograman3407
+{
+    4references
+    class Karyawan
+    {
+        4references
+        public int NIK { get; set; }
+        4references
+        public string nama { get; set; }
+        6references
+        public string gajibulanan { get; set; }
+
+        2references
+        public void bulanan()
+        {
+            Console.WriteLine("{0}{1}\t{2}", NIK, nama, gajibulanan);
+        }
+
+        2references
+        public void kenaikan ()
+        {
+            Console.WriteLine("{0}{1}\t{2}", NIK, nama, gajibulanan);
+        }
+    }
+}
